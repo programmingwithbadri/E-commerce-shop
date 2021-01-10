ecaaaafde6cf1d6b3331c304cef71416f8fc2fab
Badri Narayanan <programmingwithbadri@outlook.com>
2021-01-10 19:17:42 +0530

    Connect to DB

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const connectDb = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true
+        })
+
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    }
+    catch (err) {
+        console.error(`Error: ${err.message}`);
+        process.exit(1);
+    }
+}
+
+export default connectDb;

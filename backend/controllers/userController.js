7770bd0d37d41486b7e41f5579fbcca0dce777f9
Badri Narayanan <programmingwithbadri@outlook.com>
2021-01-23 12:11:04 +0530

    Get User profile by using auth middleware to validate jwt token from req headers

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,7 +22,25 @@ const authUser = asyncHandler(async (req, res) => {
         res.status(401);
         throw new Error('Invalid email or password')
     }
-
 })
 
-export { authUser }
+// @desc Get User profile
+// @route POST /api/users/profile
+// @access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        return res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        });
+    } else {
+        res.status(401);
+        throw new Error('User profile not found')
+    }
+})
+
+export { authUser, getUserProfile }

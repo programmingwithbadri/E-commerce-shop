8e88c449705749ad899cbdb2a98b1bdea731706c
Badri Narayanan <programmingwithbadri@outlook.com>
2021-02-05 15:30:54 +0530

    Fix the issue related with sending static files

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,35 @@
-import path from 'path';
-import express from 'express';
-import dotenv from 'dotenv';
+import path from 'path'
+import express from 'express'
+import dotenv from 'dotenv'
 import morgan from 'morgan'
-import connectDb from './config/db.js';
-import productRoutes from './routes/productRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
-import uploadRoutes from './routes/uploadRoutes.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
 
-dotenv.config();
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
-connectDb();
+dotenv.config()
 
-const app = express();
+connectDB()
+
+const app = express()
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
-app.use(express.json());
+app.use(express.json())
 
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes);
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
-app.use('/api/upload', uploadRoutes);
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
@@ -33,14 +37,23 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', index.html)))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
 }
 
-app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+app.use(notFound)
+app.use(errorHandler)
 
-app.use(notFound);
-app.use(errorHandler);
+const PORT = process.env.PORT || 5000
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} at port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+)
